26ee6fbadaf936a6a10825d7ab9cccc2b0619459
PWilliams0530 <PWilliams0530@gmail.com>
2024-05-17 16:24:50 -0400

    addedSwordHitBox

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -6,6 +6,9 @@ var gravity = 20
 var jump = 400
 var pressed = 2
 
+func _ready():
+	$Sword/CollisionShape2D.disabled = true
+
 func _physics_process(delta):
 	Move(delta)
 	velocity.y += gravity
@@ -19,11 +22,13 @@ func Move(delta):
 			velocity.x = clamp(speed,100,speed)
 			$Sprite2D.flip_h = false
 			$anim.play("Walk")
+			$Sword/CollisionShape2D.position = Vector2(0,0)
 		if movement < 0: #pressed right
 			velocity.x -= speed * delta
 			velocity.x = clamp(speed,-100,-speed)
 			$Sprite2D.flip_h = true
 			$anim.play("Walk")
+			$Sword/CollisionShape2D.position = Vector2(-50,0)
 	if movement == 0.0:
 		velocity.x = 0.0
 		$anim.play("Idle")
@@ -49,6 +54,12 @@ func Move(delta):
 	if !is_on_floor() && velocity.y > 10:
 		$anim.play("Fall")
 		
+	if Input.is_action_just_pressed("ui_sword"):
+		Sword()
+		
 func Jump():
 	$anim.play("Jump")
 	velocity.y -= jump
+
+func Sword():
+	$anim.play("Sword")
